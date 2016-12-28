9cd36f04db5d07f854b3765bbf61f2ec2d186a84
Marco Santoni <marcosantoni@hotmail.it>
2016-12-28 15:20:44 +0100

    Fix RSS feed

--- publishconf.py
+++ publishconf.py
@@ -10,11 +10,12 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://Marco-Santoni.github.io'
+SITEURL = 'http://www.marcosantoni.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+#CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = False
 
